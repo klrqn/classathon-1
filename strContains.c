9ac157d7e5bd9940aaefce968773e5bf38ae71f5
Scott Bruce Burton <scottbruceburton@gmail.com>
2018-07-26 16:20:42 +0000

    update returns and testing

--- strContains.c
+++ strContains.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <assert.h>
 
 int strContains(string, string);
+bool testStrContains(void);
 
 int main(void)
 {
@@ -11,6 +13,7 @@ int main(void)
     string a = get_string("enter an outer word: ");
     string b = get_string("enter an inner word: ");
     strContains(a, b);
+    testStrContains();
 }
 
 int strContains(string outerStr, string innerStr)
@@ -46,18 +49,43 @@ int strContains(string outerStr, string innerStr)
         }
     }
 
-    // If the 2nd string does not exist inside the first string then print:
-    // “The string does not exist”
-    // Then return the integer 0
-    // If the 2nd string exists inside the first string print:
-    // “The string exists”
-    // Then return the integer 1
-    // Ignore case.
-    // Hacker level:
-    // If the string contains the word more than once print:
-    // “This string is full of the word”
-    // Then return the integer 2
-
     printf("number of matches: %i\n", numberOfMatches);
-    return 0;
+
+    if (numberOfMatches == 0)
+    {
+        printf("The string does not exist\n");
+        return 0;
+    }
+    else if (numberOfMatches == 1)
+    {
+        printf("The string exists\n");
+        return 1;
+    }
+    else if (numberOfMatches > 1)
+    {
+        printf("This string is full of the word\n");
+        return 2;
+    }
+    else
+    {
+        printf("negative. does not compute\n");
+    }
+    return -1;
 }
+
+bool testStrContains(void)
+{
+    assert(strContains("hello", "l") == 2 && "hello has 2 l's");
+    assert(strContains("endangered", "end") == 1 && "endangered has 1 'end'");
+    assert(strContains("The dog sleeps in the dog house", "dog") == 2 && "dog is in the string twice");
+    return false;
+}
+
+// “endangered” “end” true
+// “endangered” “danger” true
+// “the dog sleeps in the dog house” “Jack” false
+// “the dog sleeps in the dog house” “dog” true (hacker multiple)
+// Hacker level 2: Confirm case
+// “The dog sleeps in the dog house” “The” single true
+// “The dog sleeps in the dog house” the” single true
+// “The dog sleeps in the dog house” “dog” multi true
